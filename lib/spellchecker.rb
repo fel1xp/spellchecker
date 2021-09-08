c2356729a4c7ed1cdf5f079d1500253260475ae4
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-09-08 15:21:56 +0100

    Spellchecker returns sentence

---
+++ lib/spellchecker.rb
@@ -0,0 +1,4 @@
+
+def spellchecker(sentence)
+  sentence
+end
--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -1,2 +1,8 @@
 require 'spellchecker'
 
+describe 'spellchecker' do 
+  it ' will return the string if it is spelt correctly' do
+    expect(spellchecker('correct')).to eq('correct')
+  end
+end
+
