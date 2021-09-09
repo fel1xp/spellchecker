1eeb9c07b6a6c7076b5177af7c9d86a29eeaced9
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-09-09 15:38:39 +0100

    First incorrect value tested

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,13 +1,14 @@
 
 
 def spellchecker(sentence)
-  wordbank = ["test1", "test2, test3"]
+  wordbank = ["test1", "test2, test3, test4, test5"]
   sentence_array = sentence.split(" ")
  
   if sentence_array.any? {|word| wordbank.include?(word) } == true
-  sentence
-end
-
+    sentence
+  else 
+    "~#{sentence}~"
+  end
 
 end
 
--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -1,6 +1,6 @@
 require 'spellchecker'
 
-describe 'spellchecker' do 
+describe 'spellchecker for correctly spelt words' do 
   it 'will return the string if it is spelt correctly' do
     expect(spellchecker('test1')).to eq('test1')
   end
@@ -12,8 +12,13 @@ describe 'spellchecker' do
   it 'will return a string of 3 correctly spelt words' do
     expect(spellchecker('test1 test2 test3')).to eq('test1 test2 test3')
   end
-  
-  
 end
+
+describe 'spellchecker for incorrectly spelt words' do 
+  it 'will return the string with ~...~ if spelt incorrectly' do
+    expect(spellchecker('incorrecttest1')).to eq('~incorrecttest1~')
+  end
+end
+
 
 
